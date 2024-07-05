facebb54a37630e41036bee722b3b2d3d35c9b09
yanarkhan <rianarkhan@gmail.com>
2024-07-06 02:33:21 +0700

    customer logo, tinggal benerin mode mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,15 @@ module.exports = {
       boxShadow: {
         custom: "0 4px 8px #c084fc",
       },
+      animation: {
+        "loop-scroll": "loop-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
